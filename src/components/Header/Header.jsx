28440dc28e9194d3c4d171fce9695affaf123cd2
Shivatyagi01 <shivaworx1@gmail.com>
2024-05-03 17:19:25 +0530

    Removed slick slider due to some error -- using [200~react-multi-carousel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,8 +23,8 @@ const Header = () => {
               className="w-1/4"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M 150.07 131.439 L 131.925 100 L 122.206 105.606 L 137.112 131.439 L 150.07 131.439 Z M 132.781 131.439 L 120.797 110.692 L 111.078 116.298 L 119.823 131.439 L 132.781 131.439 Z M 109.718 121.401 L 115.509 131.439 L 102.551 131.439 L 100 127.007 L 109.718 121.401 Z"
                 fill="black"
               ></path>
--- src/components/ProductTab/ProductTab.jsx
+++ src/components/ProductTab/ProductTab.jsx
@@ -1,143 +1,120 @@
-import React, { useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 // images
 import shoes from "../../assets/img/shoe.avif";
+
 const ProductTab = () => {
-  const [sliderIndex, setSliderIndex] = useState(0);
-
-  const CustomNextArrow = ({ onClick }) => (
-    <button
-      className="text-black right-0 text-8xl absolute border-2 border-solid border-black"
-      onClick={onClick}
-    >
-      <svg
-        width="36px"
-        height="36px"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M13.4697 5.46967C13.7626 5.17678 14.2374 5.17678 14.5303 5.46967L20.5303 11.4697C20.8232 11.7626 20.8232 12.2374 20.5303 12.5303L14.5303 18.5303C14.2374 18.8232 13.7626 18.8232 13.4697 18.5303C13.1768 18.2374 13.1768 17.7626 13.4697 17.4697L18.1893 12.75H4C3.58579 12.75 3.25 12.4142 3.25 12C3.25 11.5858 3.58579 11.25 4 11.25H18.1893L13.4697 6.53033C13.1768 6.23744 13.1768 5.76256 13.4697 5.46967Z"
-          fill="#000000"
-        />
-      </svg>
-    </button>
-  );
-
-  const CustomPrevArrow = ({ onClick }) => (
-    <button
-      className="text-black left-0 text-8xl absolute border-2 border-solid border-black"
-      onClick={onClick}
-    >
-      <svg
-        width="36px"
-        height="36px"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M10.5303 5.46967C10.8232 5.76256 10.8232 6.23744 10.5303 6.53033L5.81066 11.25H20C20.4142 11.25 20.75 11.5858 20.75 12C20.75 12.4142 20.4142 12.75 20 12.75H5.81066L10.5303 17.4697C10.8232 17.7626 10.8232 18.2374 10.5303 18.5303C10.2374 18.8232 9.76256 18.8232 9.46967 18.5303L3.46967 12.5303C3.17678 12.2374 3.17678 11.7626 3.46967 11.4697L9.46967 5.46967C9.76256 5.17678 10.2374 5.17678 10.5303 5.46967Z"
-          fill="#000000"
-        />
-      </svg>
-    </button>
-  );
-  const settings = {
-    dots: false,
-    arrows: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    nextArrow: <CustomNextArrow />,
-    prevArrow: <CustomPrevArrow />,
-    afterChange: (index) => setSliderIndex(index),
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      slidesToSlide: 4, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 767, min: 464 },
+      items: 2,
+      slidesToSlide: 1, // optional, default to 1.
+    },
   };
+  const slides = [
+    {
+      id: 1,
+      image: shoes,
+      name: "Samba OG Shoes",
+      price: 10999.0,
+      category: "shoes",
+    },
+    {
+      id: 2,
+      image: shoes,
+      name: "Samba OG Shoes",
+      price: 10999.0,
+      category: "shoes",
+    },
+    {
+      id: 3,
+      image: shoes,
+      name: "Samba OG Shoes",
+      price: 10999.0,
+      category: "shoes",
+    },
+    {
+      id: 4,
+      image: shoes,
+      name: "Samba OG Shoes",
+      price: 10999.0,
+      category: "shoes",
+    },
+    {
+      id: 5,
+      image: shoes,
+      name: "Samba OG Shoes",
+      price: 10999.0,
+      category: "shoes",
+    },
+    {
+      id: 6,
+      image: shoes,
+      name: "Samba OG Shoes",
+      price: 10999.0,
+      category: "shoes",
+    },
+  ];
   return (
-    <div className="tabs py-8">
-      <div className="container mx-auto">
-        <div className="button-group flex gap-x-4">
-          <a
-            href="#"
-            className="p-2 px-4 bg-black text-white font-custom font-bold block border-2 border-black border-solid hover:bg-white hover:text-black"
+    <>
+      <div className="tabs py-8">
+        <div className="container mx-auto">
+          <div className="button-group flex gap-x-4 pb-8">
+            <a
+              href="#"
+              className="p-2 px-4 bg-black text-white font-custom font-bold block border-2 border-black border-solid hover:bg-white hover:text-black"
+            >
+              New Arrivals
+            </a>
+            <a
+              href="#"
+              className="p-2 px-4 bg-white text-black font-custom font-bold block border-2 border-black border-solid hover:bg-black hover:text-white"
+            >
+              Best of Adidas
+            </a>
+          </div>
+          <Carousel
+            responsive={responsive}
+            autoPlay={true}
+            swipeable={true}
+            draggable={true}
+            showDots={false}
+            infinite={false}
+            partialVisible={false}
           >
-            New Arrivals
-          </a>
-          <a
-            href="#"
-            className="p-2 px-4 bg-white text-black font-custom font-bold block border-2 border-black border-solid hover:bg-black hover:text-white"
-          >
-            Best of Adidas
-          </a>
-        </div>
-        <Slider className="py-8" {...settings}>
-          <div>
-            <div className="product-box p-4">
-              <div className="product-img">
-                <img src={shoes} alt="" className="w-100" />
+            {slides.map((slide) => (
+              <div key={slide.id}>
+                <div className="product-box border-white border-2 border-solid cursor-pointer hover:border-black">
+                  <div className="product-img w-full">
+                    <img
+                      src={slide.image}
+                      alt={slide.name}
+                      className="w-full"
+                    />
+                  </div>
+                  <div className="product-cont py-2">
+                    <p>{slide.name}</p>
+                    <p>{slide.category}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div>
-            <div className="product-box p-4">
-              <div className="product-img">
-                <img src={shoes} alt="" className="w-100" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="product-box p-4">
-              <div className="product-img">
-                <img src={shoes} alt="" className="w-100" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="product-box p-4">
-              <div className="product-img">
-                <img src={shoes} alt="" className="w-100" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="product-box p-4">
-              <div className="product-img">
-                <img src={shoes} alt="" className="w-100" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="product-box p-4">
-              <div className="product-img">
-                <img src={shoes} alt="" className="w-100" />
-              </div>
-            </div>
-          </div>
-        </Slider>
-        <div style={{ marginTop: "20px" }}>
-          <SliderIndicator currentIndex={sliderIndex} totalSlides={3} />
+            ))}
+          </Carousel>
         </div>
       </div>
-    </div>
+    </>
   );
 };
-const SliderIndicator = ({ currentIndex, totalSlides }) => {
-  const indicators = [];
 
-  for (let i = 0; i < totalSlides; i++) {
-    const className =
-      i === currentIndex ? "slider-indicator active" : "slider-indicator";
-    indicators.push(<div key={i} className={className} />);
-  }
-
-  return <div className="slider-indicators">{indicators}</div>;
-};
 export default ProductTab;
